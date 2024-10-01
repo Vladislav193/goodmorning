90ae1b0bd94b15316674cd8d7b4cf4ed8f474953
Vladislav <110356212+Vladislav193@users.noreply.github.com>
2024-10-01 20:35:34 +0300

    изменил время на utc

--- app/scheduler.py
+++ app/scheduler.py
@@ -24,4 +24,4 @@ async def send_image(bot, user_id):
 
 
 def schedule_good_morning(scheduler: AsyncIOScheduler, bot):
-    scheduler.add_job(send_good_morning, 'cron', hour=9, minute=00, args=[bot])
+    scheduler.add_job(send_good_morning, 'cron', hour=6, minute=00, args=[bot])
