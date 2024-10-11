d928bed3e2aa568c82f881256926b3356c814a12
Vladislav <110356212+Vladislav193@users.noreply.github.com>
2024-10-11 15:04:23 +0300

    Добавил эмодзи

--- app/scheduler.py
+++ app/scheduler.py
@@ -7,7 +7,7 @@ users= (439845524,1978591255,)
 
 async def send_good_morning(bot):
     for user_id in users:
-        await bot.send_message(chat_id=user_id, text='Доброе утро ')
+        await bot.send_message(chat_id=user_id, text=f'\U0001F305 Доброе утро \U00002615')
         await send_image(bot, user_id)
 
 
@@ -15,7 +15,7 @@ async def send_image(bot, user_id):
     image_url = 'https://api.thecatapi.com/v1/images/search'
     get_photo = requests.get(image_url).json()
     photo = get_photo[0]['url']
-    await bot.send_photo(user_id, photo=photo, caption='А это специально для тебя')
+    await bot.send_photo(user_id, photo=photo, caption=f'А это специально для тебя \U00002764 \U0001F618')
     # async with aiohttp.ClientSession() as session:
     #     async with session.get(photo) as resp:
     #         if resp.status == 200:
